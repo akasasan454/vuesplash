90284c9c711f1a2e2ddecf53ea1f7a3ad3499c76
akasasan454 <go_aquarion.0454@icloud.com>
2019-06-12 15:10:47 +0900

    save

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require("laravel-mix");
  */
 
 mix
-  .browserSync("localhost")
+  .browserSync("localhost:8000")
   .js("resources/js/app.js", "public/js")
   .sass("resources/sass/app.scss", "public/css")
   .version();
